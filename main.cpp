1b25fae40e15748b8fd4723df46f62032acfd9af
Adam <adamthewilliams@gmail.com>
2019-02-26 09:19:40 -0600

    chaos

--- main.cpp
+++ main.cpp
@@ -23,16 +23,33 @@ typedef __uint8_t byte;
 using namespace std;
 
 struct Cmd_Options {
+    Cmd_Options(int _argc, char** _argv, int v, int b, long long os, long long oe, int s) :
+        argc{_argc}, argv{_argv}, verbose{v}, blocksize{b}, offset_start{os},
+        offset_end{oe}, searchsize{s} {}
     int        argc;
     char**     argv;
-    int        verbose;
+    bool       verbose;
     int        blocksize;
     long long  offset_start;
     long long  offset_end;
     int        searchsize;
-    char*      output_folder;
-    char*      input;
-    char*      output_file;
+    string      output_folder{""};
+    string      input{""};
+    string      output_file{""};
+
+    void print_all_options(){
+        cout << endl;
+        cout << "argc: " << argc << endl;
+        cout << "argv: " << argv << endl;
+        cout << "verbose: " << verbose << endl;
+        cout << "blocksize: " << blocksize << endl;
+        cout << "offset_start: " << offset_start << endl;
+        cout << "offset_end: " << offset_end << endl;
+        cout << "searchsize: " << searchsize << endl;
+        cout << "output_folder: " << output_folder << endl;
+        cout << "input: " << input << endl;
+        cout << "output_file: " << output_file << endl;
+    }
 };
 
 
@@ -44,11 +61,11 @@ struct Block_info {
 
 struct Arg_struct {
     Cmd_Options cmd;
-    Block_info block;
-    byte* thread_buff;
-    long long offset;
-    short thread_id;
-    short ready;
+    Block_info block{};
+    byte* thread_buff{};
+    long long offset{};
+    short thread_id{};
+    bool ready{};
 };
 
 /*
@@ -75,8 +92,20 @@ int terminate_early;
 int threads_ready;
 int threads_go;
 int* input_blocks;
+int match_count{0};
+deque<__uint8_t*> block_queue;
 
-deque<byte*> block_queue;
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -86,44 +115,41 @@ deque<byte*> block_queue;
 int main(int argc, char** argv)
 {
 
-    Cmd_Options cmds = {argc, argv, 0, 32768, 0, 0, 128, NULL, NULL, NULL};
+    Cmd_Options cmds{argc, argv, false, 32768, 0, 0, 128};
     
     threads_ready = 0;
     get_cmd_options(&cmds);    
+    
     setlocale(LC_NUMERIC, "");
 
     std::thread  thread_arr[8];
     FILE*        input_file;
     FILE*        output_file;
-    FILE*        header_file;                      
-    int          regex_i;                  
-    long long    i;
-    int          j;
+    FILE*        header_file;                                             
     int          thread_blk_sz = cmds.blocksize/8; 
-    threads_go = 0;
+    __uint8_t tmp[thread_blk_sz];
     
-    byte* buffer = new byte[cmds.blocksize];
+    __uint8_t buffer[cmds.blocksize];
     
-    if(cmds.verbose){
-        printf("\n>> Buffer alloc... %'d bytes", cmds.blocksize);
-    }    
-    cmds.output_file = new char[50];
+    if(cmds.verbose)
+        printf("\n>> Buffer alloc... %'d bytes", cmds.blocksize);        
     
     open_io_files(&input_file, &output_file, &cmds);            
+    
     Arg_struct** args = (Arg_struct**)malloc(sizeof(Arg_struct*)*8);        
     
-    for(j = 0; j < 8; ++j){
-        args[j] = (Arg_struct*)malloc(sizeof(Arg_struct));
-    }
+    for(int j = 0; j < 8; ++j)
+        args[j] = (Arg_struct*)malloc(sizeof(Arg_struct));    
     
-    for (j = 0; j < 8; ++j) {  
+    for (int j = 0; j < 8; ++j) {  
         args[j]->block;
         args[j]->cmd = cmds;
         args[j]->cmd.blocksize = cmds.blocksize/8;                                
         args[j]->thread_id = j+1;
         args[j]->offset = 1;
-        args[j]->ready = 0;
+        args[j]->ready = 0;        
         
+
         if (cmds.verbose)
             printf("\nSpawing thread #%d", j+1);
 
@@ -131,7 +157,9 @@ int main(int argc, char** argv)
         tmp.detach();
     }           
 
-    for (i = cmds.offset_start; i < cmds.offset_end; i+=cmds.blocksize ) {
+
+
+    for (long long i = cmds.offset_start; i < cmds.offset_end; i+=cmds.blocksize ) {
         
                 
         fseek(input_file, i, 0);        
@@ -139,19 +167,19 @@ int main(int argc, char** argv)
         if ((fread(buffer, 1, cmds.blocksize, input_file)) < 0) {            
             fprintf(stderr, "fread failed..");
             exit(1);
-        }      
-
-        byte tmp[thread_blk_sz];                
-        memcpy(tmp, buffer + (j*thread_blk_sz), thread_blk_sz);        
-        unique_lock<mutex> lck(mtx);        
-        block_queue.push_back(tmp);        
+        }              
         
-        if(cmds.verbose){
-            cout << "\n>> Pushed block to queue..." << endl;    
+        for(int j = 0; j < 8; ++j){
+            memcpy(tmp, buffer + (j*thread_blk_sz), thread_blk_sz);        
+            unique_lock<mutex> lck(mtx);                
+            block_queue.push_back(tmp);                            
+            if(cmds.verbose){
+                cout << "\n>> Pushed block to queue..." << endl;    
+            }
+            lck.unlock();
+            cv.notify_one();
+            cout << i << ", Queue Size: " << block_queue.size() << endl;
         }
-        lck.unlock();
-        cv.notify_one();
-        //cout << i << ", Queue Size: " << block_queue.size() << endl;
     }
         /*
         printf("\nMain thread waiting for children to be ready");
@@ -191,12 +219,26 @@ int main(int argc, char** argv)
     fclose(output_file);
     free(buffer);
     printf("\n\nSUCCESS!\n\n");
+
+    cout << "Match count: " << match_count << endl;
 }
 
 
 
 
-void add_header_info(Block_info* block, byte* transfer_info, 
+
+
+
+
+
+
+
+
+
+
+
+
+void add_header_info(Block_info* block, char* transfer_info, 
                      long long block_offset, int searchsize) {    
     block->info[block_offset] = string{(char*)transfer_info};
 }
@@ -205,22 +247,22 @@ void add_header_info(Block_info* block, byte* transfer_info,
 
 
 void open_io_files(FILE** input_file, FILE** output_file, Cmd_Options* cmds) {        
-                
-    if (!(*input_file = fopen(cmds->input, "rb"))) {
+
+    cout << "\n" << "Trying to open input: " << cmds->input << endl;          
+    if (!(*input_file = fopen(cmds->input.c_str(), "rb"))) {
         fprintf(stderr, "\n>> Input file failed to open");
         exit(1);
     }    
-    printf("\n>> Input file opened: %s", cmds->input);        
+    cout << "\n>> Input file opened: " << cmds->input << endl;        
                
-    
-    strcat(cmds->output_file, cmds->output_folder);
-    strcat(cmds->output_file, "output.txt");    
-    if (!(*output_file = fopen(cmds->output_file, "wb+"))) {
+    cmds->output_file += cmds->output_folder;
+    cmds->output_file += "output.txt";    
+    cout << "\n" << "Trying to open output_file: " << cmds->output_file.c_str() << endl;
+    if (!(*output_file = fopen(cmds->output_file.c_str(), "wb+"))) {
             fprintf(stderr, "\n>> Output file failed to open");
             exit(1);
     }    
-    printf("\n>> Output file opened: %s", cmds->output_file);
-    fflush(stdout);
+    cout << "\n>> Output file opened: " << cmds->output_file << endl;    
     
 }
 
@@ -270,13 +312,11 @@ void get_cmd_options(Cmd_Options* cmds)
             break;
         
         case 'i':
-            cmds->input = (char*)malloc(50);                
-            strcpy(cmds->input, optarg);
+            cmds->input = string{optarg};            
             break;
 
         case 'o':
-            cmds->output_folder = (char*)malloc(50);                       
-            strcpy(cmds->output_folder, optarg);
+            cmds->output_folder = string{optarg};            
             break;
         
         case 'v':
@@ -304,23 +344,26 @@ void get_cmd_options(Cmd_Options* cmds)
         }
     }
 
-    if(!cmds->output_folder) {
+    if(cmds->output_folder == "") {
         
-        cmds->output_folder = (char*)malloc(sizeof(char)*50);
-        memset(cmds->output_folder, 0, 50);  
+        char tmp_output_folder[50];
         
-        if(!(getcwd(cmds->output_folder, 50))){
+
+        if(!(getcwd(tmp_output_folder, 50)))
             printf("\nCould not retrieve directory with 'getcwd'");
-        }
+        
+        cmds->output_folder = string{tmp_output_folder};
+        
         if(cmds->verbose) {
-            strcat(cmds->output_folder, "/");
+            cmds->output_folder += '/';
             printf("\n>> Setting current directory for output: %s", 
-                   cmds->output_folder);
+                   cmds->output_folder.c_str());
         }
     }
 
     if(cmds->offset_end == 0) {
-        FILE* file_size_stream = fopen(cmds->input, "r");
+        printf("\n>> No offset end detected, setting offset to end of file");
+        FILE* file_size_stream = fopen(cmds->input.c_str(), "r");
         fseek(file_size_stream, 0, SEEK_END);
         cmds->offset_end = ftell(file_size_stream);
         fclose(file_size_stream);        
@@ -332,18 +375,17 @@ void get_cmd_options(Cmd_Options* cmds)
     
     if(cmds->verbose) {
                 
-        printf("\n>> Offset Start: %'lld bytes", cmds->offset_start);
-        printf("\n>> No offset end detected, setting offset to end of file");
+        printf("\n>> Offset Start: %'lld bytes", cmds->offset_start);        
         printf("\n>> Offset End: %'lld bytes", cmds->offset_end);
-        printf("\n>> Input file: %s", cmds->input);                 
+        printf("\n>> Input file: %s", cmds->input.c_str());                 
         printf("\n>> Input file size: %'lld bytes", filesize);
         printf("\n>> Blocksize: %'d bytes", cmds->blocksize);        
         printf("\n>> Search Size: %'d bytes", cmds->searchsize);
-        printf("\n>> Output folder: %s", cmds->output_folder);
+        printf("\n>> Output folder: %s", cmds->output_folder.c_str());
     }
 
     
-    if (!cmds->input) {
+    if (cmds->input == "") {
         printf("\n>> Input file required");
         exit(1);
     }
@@ -359,38 +401,51 @@ void sig_handler(int signal_number) {
 void search_disk(Arg_struct* args) {
 
     int j;
-    regex_t  regex;            
-    byte transfer[args->cmd.searchsize];    
-    int regex_i = regcomp(&regex, "II.{6}CR", REG_EXTENDED); 
+    regex_t regex;            
+    __uint8_t transfer[args->cmd.searchsize];   
+    //const char regex_str = "^II.{6}CR";     
     if (args->cmd.verbose) {
         printf("\n>> Transfer alloc... %'d bytes", args->cmd.searchsize);
     }
         
-    char tmp[args->cmd.blocksize];
-     
+    __uint8_t tmp[args->cmd.blocksize];
+
     for(;;){
         
         unique_lock<mutex> lck(mtx_wait);            
         cv.wait(lck, []{ return !block_queue.empty(); });
         unique_lock<mutex> queue_lck(mtx);        
         memcpy(tmp, block_queue.front(), args->cmd.blocksize);
-        block_queue.pop_front();        
+        block_queue.pop_front(); 
+        cout << "Thread " << args->thread_id << " unlocking queue" << endl;
         queue_lck.unlock();
 
         for (j = 0; j < args->cmd.blocksize; j+=args->cmd.searchsize) {  
-            memcpy(transfer, tmp+j, args->cmd.searchsize);
+            memcpy(transfer, &tmp[j], args->cmd.searchsize);
+            int regex_i = regcomp(&regex, "^II.{6}CR", REG_EXTENDED); 
             regex_i = regexec(&regex, (const char*)transfer, 0, NULL, 0);
             
             if(args->cmd.verbose){
                 cout << "\n>> Thread #" << args->thread_id 
-                    << "searching for match" << endl;
+                    << " searching for match" << endl;
             }
-
-            cout << hex << transfer << endl;
+            
+            if(args->cmd.verbose) {
+                for(int i = 0; i < args->cmd.searchsize; ++i){
+                    cout << hex << transfer[i];
+                }
+                cout << endl;
+                cout << "Size of transfer: " << sizeof(transfer) << endl;
+            }
+            
             if(!regex_i) {
+                unique_lock<mutex> lck(mtx_wait);
                 printf("\nMATCH!");            
-                printf("\n>> Thread #%d adding headers to table", args->thread_id);                
-                add_header_info(&args->block, transfer, args->offset, args->cmd.searchsize);
+                printf("\n>> Thread #%d adding headers to table", args->thread_id);
+                fflush(stdout);
+                match_count++;
+                lck.unlock();
+                //add_header_info(&args->block, transfer, args->offset, args->cmd.searchsize);
             }        
         }  
     }
